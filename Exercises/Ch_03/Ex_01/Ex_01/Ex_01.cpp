566ad674b715e79e865202fdd873239cdbc8ba45
grahfoster <grahfoster@gmail.com>
2019-03-29 20:05:40 -0500

    Exercise 3.01 completed

---
+++ Exercises/Ch_03/Ex_01/Ex_01/Ex_01.cpp
@@ -0,0 +1,55 @@
+#include "../../../../std_lib_facilities.h"
+
+int main() {
+	//cout << "Please enter your first_name and age\n";
+	//string first_name = "???";	// string variable
+	//							// ("???" means "don't know the name")
+	//double age = -1;	// integer variable (-1 means "don't know the age")
+	//cin >> first_name >> age;	// read a string followed by an integer
+	//cout << "Hello, " << first_name << " (age " << age * 12 << " months)\n ";
+
+	///
+
+	//cout << "Please enter a floating-point value: ";
+	//int n;
+	//cin >> n;
+	//double d = n;
+	//cout << "n == " << n
+	//	<< "\nn+1 == " << n + 1
+	//	<< "\nthree times n == " << 3 * n
+	//	<< "\ntwice n == " << n + n
+	//	<< "\nn squared = " << n * n
+	//	<< "\nhalf of n == " << d / 2
+	//	<< "\nsquare root of n == " << sqrt(d)
+	//	<< endl;	// another name for newline ("end of line")
+
+	///
+
+	//string previous = " ";	// previous word; initialized to "not a word"
+	//string current;	// current word
+	//while (cin >> current) {
+	//	// read a stream of words
+	//	if (previous == current) // check if the word is the same as last
+	//		cout << "repeated word: " << current << '\n';
+	//	previous = current;
+	//}
+
+	///
+
+	/*string s = "Goodbye, cruel world! ";
+	cout << s << '\n';*/
+
+	///
+
+	//double d = 0;
+	//while (cin >> d) {	// repeat the statements below
+	//					// as long as we type in numbers
+	//	int i = d;	// try to squeeze a double into an int
+	//	char c = i;	// try to squeeze an int into a char
+	//	int i2 = c;	// get the integer value of the character
+	//	cout << "d==" << d	// the original double
+	//		<< " i=" << i	// converted to int
+	//		<< " i2=" << i2	// int value of char
+	//		<< " char(" << c << ")\n";	// the char
+	//}
+}
