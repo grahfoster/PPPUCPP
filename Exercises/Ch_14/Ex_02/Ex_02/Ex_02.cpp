ed3020e5d2f2629935d93d4e87e39c8469c489dd
grahfoster <grahfoster@gmail.com>
2019-05-30 17:25:26 -0500

    Exercise 14.02 completed

---
+++ Exercises/Ch_14/Ex_02/Ex_02/Ex_02.cpp
@@ -0,0 +1,24 @@
+#include "../Graph.h"
+#include "../Simple_window.h"
+
+int main()
+try {
+	using namespace Graph_lib;
+
+	const Point win_tl{ x_max() / 4, y_max() / 4 };
+	const int win_width = x_max() / 2;
+	const int win_height = y_max() / 2;
+	const string win_name{ "My window" };
+	Simple_window s_win{ win_tl, win_width, win_height, win_name };
+
+	Circle c1{ { 100, 100 }, 50 };
+	Circle c2{ c1 };
+
+	s_win.wait_for_button();
+}
+catch (exception& e) {
+	cerr << "Error: " << e.what() << ".\n";
+}
+catch (...) {
+	cerr << "Unexpected error.\n";
+}
