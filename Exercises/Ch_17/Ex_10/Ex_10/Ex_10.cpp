236d2c9c75f469df26529339b4d8ed1289800e5d
grahfoster <grahfoster@gmail.com>
2019-06-25 11:45:31 -0500

    Exercise 17.10 completed

---
+++ Exercises/Ch_17/Ex_10/Ex_10/Ex_10.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main()
+try {
+	const char terminator = '!';
+
+	int size = 1;
+
+	char* s = new char[size];
+	for (int i = 0; std::cin; ++i) {
+		char c = std::cin.get();
+		if (c == terminator || std::cin.eof()) {
+			size = i;
+			break;
+		}
+		s[i] = c;
+	}
+
+	for (int i = 0; i < size; ++i)
+		std::cout << s[i];
+	std::cout << '\n';
+
+	delete[] s;
+}
+catch (std::exception& e) {
+	std::cerr << "Error: " << e.what() << ".\n";
+}
+catch (...) {
+	std::cerr << "Unexpected error.\n";
+}
