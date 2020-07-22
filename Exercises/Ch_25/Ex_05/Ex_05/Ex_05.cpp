117367f8d17c6c829e1a3051d457795c78394e18
grahfoster <grahfoster@gmail.com>
2020-07-22 16:42:26 -0400

    Exercise 25.05 completed

---
+++ Exercises/Ch_25/Ex_05/Ex_05/Ex_05.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+try {
+	while (true);
+}
+catch (std::exception& e) {
+	std::cerr << "Error: " << e.what() << ".\n";
+}
+catch (...) {
+	std::cerr << "Unexpected error.\n";
+}
